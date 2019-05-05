40d8e5e483c2a6ef493896a7151a70dafe46dccf
mostafa sadeghi <mostafasadeghi97@gmail.com>
2019-05-05 11:30:48 +0430

    khabaronline crawler reformed

--- news_crawler/news_crawler/spiders/khabaronline.py
+++ news_crawler/news_crawler/spiders/khabaronline.py
@@ -20,20 +20,28 @@ class KhabaronlineSpider(Spider):
         string = 'https://www.khabaronline.ir/archive?tp='
         for num in range(1,14):
             url = string + str(num)
-            yield Request(url, callback=self.parse_category)
+            yield Request(url, callback=self.parse_category,meta={'category':num})
 
     def parse_category(self, response):
 
         news_links = response.xpath('//*[@class="desc"]/h3/a/@href').extract()
         for link in news_links:
             url = 'http://khabaronline.ir' + link
-            yield Request(url, callback=self.parse_news)
+            yield Request(url, callback=self.parse_news, meta={'category':response.meta.get('category')})
         next_page_url = response.xpath('//*[@class="page-link"]/@href').extract()[-1]
         next_page_url = 'http://khabaronline.ir' + next_page_url
-        yield Request(next_page_url, callback=self.parse_category)
+        # yield Request(next_page_url, callback=self.parse_category)
 
     def parse_news(self, response):
         title = response.xpath('//*[@class="title"]/a/text()').extract_first()
         summary = response.xpath('//*[@class="summary introtext"]/text()').extract_first()
-        content = response.xpath('//*[@class="item-text"]/div').extract_first()
+        date = response.xpath('//*[@class="col-6 col-sm-4 item-date"]/span/text()').extract_first()
+        a = response.xpath('//*[@class="item-body"]//div//text()').extract()
+        content=''
+        for i in a:
+            content+=i
+        
+        yield {'category':response.meta.get('category'),'title':title,'conetent':content}
+
+
 
