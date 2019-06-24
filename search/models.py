28f8b966635cf56acd4bc0cf7ed68d2bb62275d4
mostafa sadeghi <mostafasadeghi97@gmail.com>
2019-06-24 17:37:48 +0430

    add additional fileds to document model

--- search/models.py
+++ search/models.py
@@ -8,6 +8,12 @@ class Document(models.Model):
     title = models.CharField(max_length=300, null=True, blank=True)
     summary = models.TextField(null=True,blank=True)
     content = models.TextField(null=True, blank=True)
+    date = models.CharField(max_length=200, null=True,blank=True)
+    image_link = models.TextField(null=True,blank=True)
+
+
+    def __str__(self):
+        return str(self.title)
 
 # Store term frequency in a document
 # The score is calculated at the end
@@ -18,9 +24,16 @@ class TermFrequency(models.Model):
     frequency = models.IntegerField(default=1)
     score = models.FloatField(default=0)
 
+
+    def __str__(self):
+        return (str(self.term)+ " repeated ({}) times".format(self.frequency))
+
     
 # Store how many document contains a given
 # term
 class DocFrequency(models.Model):
     term = models.CharField(max_length=200, null=True, blank=True)
     num_docs = models.IntegerField(default=1)
+
+    def __str__(self):
+        return (str(self.term)+ " in ({}) documents".format(self.num_docs))
