5630573fb0429dca906959cdb589c610a18d3fb9
mostafa sadeghi <mostafasadeghi97@gmail.com>
2019-06-26 16:17:45 +0430

    add views of front app

--- front_view/views.py
+++ front_view/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from search.models import Document
+from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'front_view/index.html')
+    query = Document.objects.all().values('title','date','category','image_link')[:6]
+    pop_query = Document.objects.all().values('title','date','category','image_link','summary')[:90:10]
+    
+    return render(request, 'front_view/index.html',{'news':query,'popular_news':pop_query})
+
+
+def category(request, category_id):
+    news = Document.objects.filter(category=category_id).values('title','date','summary')
+    return JsonResponse({'news':news})
--- search/views.py
+++ search/views.py
@@ -1,7 +1,22 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render , get_object_or_404
 from search.suggest import rank, suggessted_terms
 import json
+from .models import Document, DocFrequency, TermFrequency
+
+
+def get_by_category(reqHttpResponseuest, category_id):
+    news = Document.objects.filter(category=category_id).values('summary','title')
+    return JsonResponse({'news':news})
+
+
+def get_detail_news(request, news_id):
+    news = get_object_or_404(Document, id=news_id)
+    return JsonResponse({'news':news})
+
+
+
+
 
 def search_term(request):
     query = request.GET.get('query')
@@ -13,8 +28,9 @@ def search_term(request):
     return render(request, 'search/home.html', context)
 
 def term_autocomplete(request):
+    print(request.GET)
     term = request.GET.get('query')
-    suggested_terms = search_term(term)
+    suggested_terms = suggessted_terms(term)
     result = {}
     result['suggestions'] = suggested_terms
-    return HttpResponse(json.dumps(result))
+    return JsonResponse(result)
